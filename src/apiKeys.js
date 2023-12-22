24e8404c3149bd3c8f8f6dfeb537a3cb3a1c140d
Karan Gupta <karanvirgupta33888@gmail.com>
2023-12-22 18:16:17 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2e97fc09658390ebfcbaea2f095edd33",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
